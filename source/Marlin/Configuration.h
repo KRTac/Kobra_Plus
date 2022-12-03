3d97171e5fa28e3697ae9c647006537668db5d6b
Dino Krtanjek <krtanjekdino@gmail.com>
2022-12-03 16:36:02 +0100

    Set unified probing margin

--- source/Marlin/Configuration.h
+++ source/Marlin/Configuration.h
@@ -1022,11 +1022,11 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-//#define PROBING_MARGIN 10
-#define PROBING_MARGIN_LEFT  4
-#define PROBING_MARGIN_RIGHT 1
-#define PROBING_MARGIN_FRONT 2
-#define PROBING_MARGIN_BACK  0
+#define PROBING_MARGIN 5
+//#define PROBING_MARGIN_LEFT  4
+//#define PROBING_MARGIN_RIGHT 1
+//#define PROBING_MARGIN_FRONT 2
+//#define PROBING_MARGIN_BACK  0
 
 
 // X and Y axis travel speed (mm/min) between probes
