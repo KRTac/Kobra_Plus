cf53fab29ebcb84483bab57c39325edae4c90c8b
Dino Krtanjek <krtanjekdino@gmail.com>
2023-01-09 23:30:40 +0100

    Default extruder steps for Orbiter v2

--- source/Marlin/Configuration.h
+++ source/Marlin/Configuration.h
@@ -767,7 +767,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 100, 400, 430 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 100, 400, 670 }
 
 /**
  * Default Max Feed Rate (mm/s)
